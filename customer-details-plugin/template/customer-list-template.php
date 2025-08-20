30fa4a6b07dd226147d423262c30990cf7ff4697
Hashil-del <nhagadgets@gmail.com>
2025-08-20 15:04:23 +0530

    Add files via upload

---
+++ customer-details-plugin/template/customer-list-template.php
@@ -0,0 +1,6 @@
+<?php if(!defined('ABSPATH')) exit; ?>
+
+<div id="cdp-customer-list">
+    <input type="text" id="cdp-search" placeholder="Search Customers...">
+    <div id="cdp-results"></div>
+</div>
