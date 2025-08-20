6e95ac75a5a362b9319e5fbe649b0a543e371265
Hashil-del <nhagadgets@gmail.com>
2025-08-20 15:10:30 +0530

    Delete customer-details-plugin directory

--- customer-details-plugin/template/customer-list-template.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined('ABSPATH')) exit; ?>
-
-<div id="cdp-customer-list">
-    <input type="text" id="cdp-search" placeholder="Search Customers...">
-    <div id="cdp-results"></div>
-</div>
